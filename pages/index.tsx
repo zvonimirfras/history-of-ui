b4cdeaec81f812a7532a171559c1510eee1f899b
zvonimirfras <zvonimir@zvonimirfras.com>
2022-10-21 21:55:20 -0400

    Fix event tile spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -126,7 +126,7 @@ const Home: NextPage = () => {
 				<div className='box-border relative mt-8 mb-8 before:bg-slate-300 before:bottom-0 before:content-[""] before:left-1/2 before:-ml-1 before:absolute before:top-0 before:w-1 before:z-1'>
 					<div className='relative z-2'>
 						{
-							allEvents.sort((a, b) => a.time < b.time ? -1 : 1).map((event, index) => <div className={`${index % 2 ? 'left-1/2 after:-left-3' : 'left-0 pl-3 pr-8'} text-base pl-8 pr-3 pt-10 w-1/2 z-2 relative after:border-4 after:border-slate-300 after:rounded-full after:content[""] after:h-5 after:w-5 after:absolute after:-right-2 after:top-1/2 after:bg-white after:translate-y-1/2 after:z-1`}>
+							allEvents.sort((a, b) => a.time < b.time ? -1 : 1).map((event, index) => <div className={`${index % 2 ? 'left-1/2 after:-left-3 pl-8 pr-5' : 'left-0 pl-5 pr-8'} text-base pt-10 w-1/2 z-2 relative after:border-4 after:border-slate-300 after:rounded-full after:content[""] after:h-5 after:w-5 after:absolute after:-right-2 after:top-1/2 after:bg-white after:translate-y-1/2 after:z-1`}>
 								<EventTile event={event} />
 							</div>)
 						}
