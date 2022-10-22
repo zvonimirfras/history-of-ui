e707d7b9b16ce98a974434b85fd9c9e82c7a6a95
zvonimirfras <zvonimir@zvonimirfras.com>
2022-10-21 21:47:31 -0400

    Add nextjs assetPrefix, disable image optimization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	assetPrefix: isProd ? '/' : '',
+	images: {
+		unoptimized: true
+	}
+};
+
+module.exports = nextConfig;
