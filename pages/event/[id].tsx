69ebef8461fe58ca7c2ac560e2f5d9328a749cc3
zvonimirfras <zvonimir@zvonimirfras.com>
2022-10-21 22:20:21 -0400

    Display useful information in event pages

--- pages/event/[id].tsx
+++ pages/event/[id].tsx
@@ -1,11 +1,23 @@
 import { useRouter } from "next/router";
+import { events } from "../../components/events";
+import { prettyDate } from "../../tools";
 
 const Event = () => {
 	const router = useRouter();
 	const { id } = router.query;
 
-	return <div>
-		{id}
+	const event = events.find((event) => event.id === id);
+
+	return <div className='p-8'>
+		{
+			!event
+			? "Don't know about that event."
+			: <>
+				<h1 className='text-3xl mb-4 font-bold'>{event.name}</h1>
+				<h2 className='text-2xl mb-2'>{prettyDate(event.time)}</h2>
+				<section>{event.description || event.summary}</section>
+			</>
+		}
 	</div>;
 };
 
