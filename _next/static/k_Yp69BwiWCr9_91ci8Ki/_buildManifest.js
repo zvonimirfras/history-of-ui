30ba0cf63737f8bf0027f7265733ddb63621a9db
zvonimirfras <zvonimir.fras@gmail.com>
2022-11-06 17:22:07 +0000

    Deploying to gh-pages from @ zvonimirfras/history-of-ui@8badf46698696eaebbac5899f68b26388e2f7f97 🚀

--- _next/static/k_Yp69BwiWCr9_91ci8Ki/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e597d17faf5bcec1.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/event/[id]":["static/chunks/pages/event/[id]-14d2b20ecdaa41c0.js"],sortedPages:["/","/_app","/_error","/event/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
