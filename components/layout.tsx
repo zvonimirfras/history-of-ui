672a7a2a1d07396c2d84e4e8fef540e7f5f3fa07
zvonimirfras <zvonimir@zvonimirfras.com>
2022-11-06 12:20:27 -0500

    Use modal component for form modal

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
-import { Close, RequestQuote } from '@carbon/icons-react';
+import { RequestQuote } from '@carbon/icons-react';
 import { useState } from 'react';
+import { Modal } from './modal';
 
 const Layout = ({ children }: any) => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
@@ -28,27 +29,13 @@ const Layout = ({ children }: any) => {
 				<RequestQuote size='24' />
 			</button>
 
-			{
-				isModalOpen
-				&& <div className='relative z-10' aria-labelledby='modal-title' role='dialog' aria-modal='true'>
-					<div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity'></div>
-
-					<div className='fixed inset-0 overflow-y-auto'>
-						<div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
-							<div className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all w-[calc(100vw-4rem)]  max-w-3xl h-[calc(100vh-4rem)]'>
-								<button
-								onClick={() => setIsModalOpen(false)}
-								className='absolute right-0 top-0 p-2 bg-white'>
-									<Close size='32' />
-								</button>
-								<div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
-									<iframe className='w-[calc(100vw-7rem)] h-[calc(100vh-7rem)]  max-w-full' src='https://docs.google.com/forms/d/e/1FAIpQLSffPwmzEMwPRB9jMPzKYbN-doJ__YHRf7C1FBwHcnVZwmnwqg/viewform?embedded=true'>Loading…</iframe>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			}
+			<Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+				<iframe
+				className='w-[calc(100vw-7rem)] h-[calc(100vh-7rem)]  max-w-full'
+				src='https://docs.google.com/forms/d/e/1FAIpQLSffPwmzEMwPRB9jMPzKYbN-doJ__YHRf7C1FBwHcnVZwmnwqg/viewform?embedded=true'>
+					Loading…
+				</iframe>
+			</Modal>
 
 			<footer>
 
