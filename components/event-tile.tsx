67ce81a429ca4d1c94ad4e863e2849ec538f0d1a
zvonimirfras <zvonimir@zvonimirfras.com>
2022-11-06 18:02:26 -0500

    Improve timeline rensponsiveness

--- components/event-tile.tsx
+++ components/event-tile.tsx
@@ -7,10 +7,22 @@ export const EventTile = ({ event, side='left' }: any) => {
 		className='rounded-lg mt-2 shadow-sm border-gray p-4 border-2 hover:shadow-md hover:shadow-slate-500 active:shadow-sm transition-all block relative bg-white'>
 			<h2 className='text-xl font-bold mb-4'>{prettyDate(event.time)}</h2>
 			<h3 className='text-lg mb-2'>{event.name}</h3>
-			<div className='flex'>
-				{ side === 'left' && event.thumbnail && <img src={event.thumbnail} alt={event.thumbnailAlt} className='mr-6' />}
+			<div className='flex flex-col xl:flex-row'>
+				{
+					side === 'left' && event.thumbnail
+					&& <img
+						src={event.thumbnail}
+						alt={event.thumbnailAlt}
+						className='h-fit w-fit m-auto mb-6 xl:mr-6 xl:mt-auto xl:mb-auto' />
+				}
 				{event.summary}
-				{ side === 'right' && event.thumbnail && <img src={event.thumbnail} alt={event.thumbnailAlt} className='ml-6' />}
+				{
+					side === 'right' && event.thumbnail
+					&& <img
+						src={event.thumbnail}
+						alt={event.thumbnailAlt}
+						className='h-fit w-fit m-auto mt-6 xl:ml-6 xl:mt-auto xl:mb-auto' />
+				}
 			</div>
 		</a>
 	</Link>;
