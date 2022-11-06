8badf46698696eaebbac5899f68b26388e2f7f97
zvonimirfras <zvonimir@zvonimirfras.com>
2022-11-06 12:20:45 -0500

    Show full size image in a modal

--- components/image.tsx
+++ components/image.tsx
@@ -1,3 +1,23 @@
+import { useState } from "react";
+import { Modal } from "./modal";
+
 export const Image = ({ src, srcFull, alt, className }: any) => {
-	return <img src={src} alt={alt} className={`m-auto mb-6 ${className}`} />;
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	return <>
+		<img
+			src={src || srcFull}
+			alt={alt}
+			tabIndex={0}
+			className={`m-auto mb-6 cursor-pointer ${className}`}
+			onClick={() => setIsModalOpen(true)} />;
+
+		<Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+			<img src={srcFull || src} />
+			{
+				alt
+				&& <div className='mt-3 text-center'>{alt}</div>
+			}
+		</Modal>
+	</>;
 };
