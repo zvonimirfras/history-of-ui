0e70af125bc96b5b37384bb965b3606496f22992
zvonimirfras <zvonimir@zvonimirfras.com>
2025-11-30 18:59:39 -0500

    Make Image alt text lint-compliant

--- components/image.tsx
+++ components/image.tsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
-import { Modal } from "./modal";
+import { useState } from 'react';
+import { Modal } from './modal';
 
 export const Image = ({ src, srcFull, alt, className }: any) => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	return <>
+		{/* eslint-disable-next-line @next/next/no-img-element */}
 		<img
 			src={src || srcFull}
-			alt={alt}
+			alt={alt || ''}
 			tabIndex={0}
 			className={`m-auto mb-6 cursor-pointer ${className}`}
 			onClick={() => setIsModalOpen(true)} />
 
 		<Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
-			<img src={srcFull || src} className='m-auto max-h-[calc(100vh-9rem)]' />
+			{/* eslint-disable-next-line @next/next/no-img-element */}
+			<img src={srcFull || src} alt={alt || ''} className='m-auto max-h-[calc(100vh-9rem)]' />
 			{
 				alt
 				&& <div className='mt-3 text-center'>{alt}</div>
